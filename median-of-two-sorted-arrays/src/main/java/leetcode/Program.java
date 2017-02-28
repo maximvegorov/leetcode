3577af8986422baca6f66613998f16ef68d7fc00
Maxim Egorov <egorovmv@yandex-team.ru>
2017-02-28 20:05:58 +0300

    Solved median-of-two-sorted-arrays

--- median-of-two-sorted-arrays/src/main/java/leetcode/Program.java
+++ median-of-two-sorted-arrays/src/main/java/leetcode/Program.java
@@ -69,7 +69,7 @@ public class Program {
             return null;
         }
 
-        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double findMedianSortedArrays(int[] nums1, int[] nums2) {
             if (nums1.length == 0 && nums2.length == 0) {
                 return 0;
             } else if (nums1.length == 0) {
