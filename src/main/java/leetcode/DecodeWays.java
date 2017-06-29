f779cf1e829397bf4afb1790ab1e10f8c4026c5d
Maxim Egorov <egorovmv@yandex-team.ru>
2017-06-29 13:16:26 +0300

    decode-ways

---
+++ src/main/java/leetcode/DecodeWays.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class DecodeWays {
+    public static void main(String[] args) {
+        System.out.println(new Solution().numDecodings("12"));
+    }
+
+    public static final class Solution {
+        public int numDecodings(String s) {
+            return 0;
+        }
+    }
+}
