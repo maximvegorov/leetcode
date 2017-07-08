a2ec8cc7bd90433fcdb34e7f59781171d2b944ec
Maxim Egorov <egorovmv@yandex-team.ru>
2017-07-09 01:07:28 +0300

    concatenated-words

--- src/main/java/leetcode/ConcatenatedWords.java
+++ src/main/java/leetcode/ConcatenatedWords.java
@@ -13,24 +13,57 @@ public final class ConcatenatedWords {
 
     public static final class Solution {
         public List<String> findAllConcatenatedWordsInADict(String[] words) {
-            return Arrays.stream(words)
-                    .parallel()
-                    .map(w1 -> new ConcatenableWord(
-                            w1,
-                            Arrays.stream(words)
-                                    .filter(w2 -> w2.length() < w1.length() && w1.contains(w2))
-                                    .sorted(Comparator.comparing(String::length))
+            List<WordWithFreqs> wordsWithFreqs = Arrays.stream(words)
+                    .map(WordWithFreqs::new)
+                    .sorted(Comparator.comparingInt(w -> w.word.length()))
+                    .collect(Collectors.toList());
+            return wordsWithFreqs
+                    .stream()
+                    .map(w1 -> new WordWithSubWords(
+                            w1.getWord(),
+                            wordsWithFreqs.stream()
+                                    .filter(w1::isCandidate)
+                                    .map(WordWithFreqs::getWord)
                                     .collect(Collectors.toList())))
-                    .filter(ConcatenableWord::isConcatenable)
-                    .map(ConcatenableWord::getWord)
+                    .filter(WordWithSubWords::isConcatenable)
+                    .map(WordWithSubWords::getWord)
                     .collect(Collectors.toList());
         }
 
-        private static final class ConcatenableWord {
+        private static final class WordWithFreqs {
+            final String word;
+            final short[] freqs;
+
+            WordWithFreqs(String word) {
+                this.word = word;
+                this.freqs = new short[26];
+                for (int i = 0; i < word.length(); i++) {
+                    freqs[word.charAt(i) - 'a']++;
+                }
+            }
+
+            public String getWord() {
+                return word;
+            }
+
+            boolean isCandidate(WordWithFreqs wordWithFreqs) {
+                if (word.length() <= wordWithFreqs.word.length()) {
+                    return false;
+                }
+                for (int i = 0; i < freqs.length; i++) {
+                    if (freqs[i] < wordWithFreqs.freqs[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        private static final class WordWithSubWords {
             final String word;
             final List<String> candidates;
 
-            ConcatenableWord(String word, List<String> candidates) {
+            WordWithSubWords(String word, List<String> candidates) {
                 this.word = word;
                 this.candidates = candidates;
             }
